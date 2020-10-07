8599e4e251e94c380d092468149e3274b3a31567
Korhal <ninj4Noob@hotmail.com>
2020-10-07 16:30:52 -0300

    First commit

---
+++ Assets/SimpleNoiseFilter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleNoiseFilter : INoiseFilter{
+	NoiseSettings.SimpleNoiseSettings settings;
+	Noise noise = new Noise();
+
+	public SimpleNoiseFilter(NoiseSettings.SimpleNoiseSettings settings){
+		this.settings = settings;
+	}
+
+	public float Evaluate(Vector3 point){
+		float noiseValue = 0;
+		float frequency = settings.baseRoughness;
+		float amplitute = 1;
+		for (int i = 0; i < settings.numLayers; i++){
+			float v = noise.Evaluate(point * frequency + settings.centre);
+			noiseValue += (v + 1) * 0.5f * amplitute;
+			frequency *= settings.roughness;
+			amplitute *= settings.persistance;
+		}
+
+		noiseValue = noiseValue - settings.minValue;
+		return noiseValue * settings.strength;
+	}
+}
